82431c750f5e3a9a9a60da2597fcc247823a5c57
TarasFirst <firsttaras@gmail.com>
2025-02-17 13:09:26 +0100

    Fix test_main_rout: - change "Welcome" to "Hello"

--- tests/test_main_route.py
+++ tests/test_main_route.py
@@ -8,7 +8,8 @@ def client():
     with app.test_client() as client:
         yield client
 
+
 def test_main_route(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Welcome" in response.data
+    assert b"Hello" in response.data
